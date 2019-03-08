0019b1dde3d25b433807e528f1a36103839b4975
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-03-08 09:35:51 -0500

    Tarea de Funciones

--- Semana3/Dia4/funciones3.js
+++ Semana3/Dia4/funciones3.js
@@ -18,4 +18,19 @@ console.log(`res2 => ${multiplicar(4,5)}`);
 function sumar(a,b){
     return a+b;
 }
+// 3,6,7,9
+console.log(`Resultado => ${sumar(sumar(3,6),sumar(7,9))}`);
+
+// Realizar una funcion que dados 2 parámetros (a y b)
+// Calcule la multiplicación de ambos sin usar el signo 
+//  * 
+// solo se pueden usar sumas y restas
+// la funcion debe retornar el producto
+
+
+// Realizar una funcion que dados el dividendo y el divisor
+// Calcule la division entera  + el residuo(si tuviera)
+// sin usar el signo / ni  % ni mod ni nada
+// solo sumas y/o restas
+// la funcion debe retornar el cociente y el residuo
 
