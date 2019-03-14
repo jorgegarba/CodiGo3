5a0554a5981df4a38654348282d86c096e40be50
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-03-14 10:29:07 -0500

    Elementos2 y Tarea

--- Semana4/Dia3/element2.js
+++ Semana4/Dia3/element2.js
@@ -7,3 +7,18 @@ parrafo.style.color = "blue";
 // agrega al elemento1, el elemento2
 // al final de todos sus hijos directos
 xprincipal.appendChild(parrafo);
+
+for(let i=0 ; i<5; i++){
+    let miParrafo = document.createElement("p");
+    miParrafo.innerHTML = `Texto del Párrafo ${i+1}`;
+    xprincipal.appendChild(miParrafo);
+}
+
+// Crear una tabla de n elementos
+// Cada fila tendrá los campos 
+// nro (numeración automática)
+// cantidad (ingresado por el usuario)
+// descripción (ingresado por el usuario)
+// P.unitario (ingresado por el usuario)
+// P.total (calculado por el sistema).
+// Al final de la tabla mostrar el total de la compra
