dac4df09deb4d6a873b3cb563466d3f74f8f2f59
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-03-18 15:32:41 -0500

    LocalStorage Tarea

---
+++ Semana5/Dia1/localstorage.js
@@ -0,0 +1,36 @@
+window.addEventListener("load",function(){
+    //localStorage => Objecto creado por defecto
+    //que sirve para guardar datos en el navegador
+    //OBS => Los datos se pierden cuando el navegador es cerrado.
+    //setItem([clave],[valor])=>
+    // clave => es el nombre de la variable que se creará en el localStorage
+    // valor => es el valor que le pertenecerá a dicha clave.
+    localStorage.setItem("nombre","Jorge Luis");
+    //getItem([clave])=> obtiene un valor dada una clave
+    document.querySelector("body").innerHTML = localStorage.getItem("nombre");
+    //removeItem([clave])=> borra una clave del LocalStorage
+    localStorage.removeItem("nombre");
+    // Creando un objeto que represente a una factura
+    let factura = {
+        fecha:'18.03.2019',
+        total:'S/.1900.00',
+        productos: [{
+                        nombre:"Cocina"
+                    },
+                    {
+                        nombre:"Lavadora"
+                    },
+                    {
+                        nombre:"Refrigerador"
+                    }]
+    };
+    // JSON.stringify([objeto])=> convierte el objeto a una cadena o string
+    // Es necesario realizar este paso ya que el localStora, acepta strings solamente
+    let facturaString = JSON.stringify(factura);
+    localStorage.setItem("factura",facturaString);
+
+    // JSON.parse([string]) => convierte una cadena o string a un objeto JSON
+    let facturaStorageString = localStorage.getItem("factura");
+    let facturaStorageJSON = JSON.parse(facturaStorageString);
+    console.log(facturaStorageJSON);
+});
