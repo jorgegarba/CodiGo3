296187e79b20277ecdecd71497e196a71541f334
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-06-07 15:31:40 -0500

    Drawer

--- Semana17/codigo/application/navigations/logged.js
+++ Semana17/codigo/application/navigations/logged.js
@@ -11,5 +11,44 @@ const playasStackNavigator = createStackNavigator(
         miPlayasScreen:{
             screen: PlayasScreen
         }
+    },
+    {
+        initialRouteName: 'miPlayasScreen',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'rgba(30,30,30,1)'
+            },
+            headerTitleStyle: {
+                textAlign: 'center',
+                alignSelf: 'center',
+                fontSize: 20,
+                color: '#fff',
+                fontWeight: 'bold',
+                flex: 1
+            }
+        }
     }
-)
+);
+
+const drawerNavigator = createDrawerNavigator(
+    {
+        miPlayasStackNavigator : {
+            screen: playasStackNavigator
+        },
+    },
+    {
+        initialRouteName:'miPlayasStackNavigator',
+        drawerBackgroundColor: 'rgba(128,35,60,0.7)',
+        contentOptions:{
+            activeTintColor: 'white',
+            activeBackgroundColor: 'transparent',
+            inactiveTintColor: 'white',
+            itemsContainerStyle:{
+                marginVertical:0
+            }
+        }
+    }
+);
+
+export default createAppContainer(drawerNavigator);
+
